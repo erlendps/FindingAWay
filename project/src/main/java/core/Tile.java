834e1c26ca46903bd251e91090b98ee01fc69129
Erlend Skaaden <erlendskaaden@gmail.com>
2021-02-12 13:01:46 +0100

    Add functionality to Tile class.
    Several methods for accessing the state of the tile and chaning its
    type. One cannot change the tiles coordinates.

--- project/src/main/java/core/Tile.java
+++ project/src/main/java/core/Tile.java
@@ -1,5 +1,58 @@
 package core;
 
 public class Tile {
-
+	private int x;
+	private int y;
+	private char type; 
+	/*
+	 * For the type, these are the different (allowed) types:
+	 * ' ' = air
+	 * '#' = ground/grass
+	 * '*' = goal/finish
+	 * 'B' = boxes (moveable)
+	 * '=' = water
+	 * 'o' = player head
+	 */
+	
+	public Tile(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.type = ' ';
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public char getType() {
+		return type;
+	}
+	
+	public void setAir() {
+		type = ' ';
+	}
+	
+	public void setGround() {
+		type = '#';
+	}
+	
+	public void setFinish() {
+		type = '*';
+	}
+	
+	public void setBox() {
+		type = 'B';
+	}
+	
+	public void setWater() {
+		type = '=';
+	}
+	
+	public void setPlayer() {
+		type = 'o';
+	}
 }
