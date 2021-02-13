283d2b59e6a8cea3a2b5b90fbd38d2833167e751
Erlend Skaaden <erlendskaaden@gmail.com>
2021-02-13 19:45:35 +0100

    Add funtionality to Game class
    Mostly getters and some checkers

--- project/src/main/java/core/Game.java
+++ project/src/main/java/core/Game.java
@@ -1,5 +1,34 @@
 package core;
 
 public class Game {
-
+	private int height;
+	private int width;
+	private int[][] board;
+	
+	public Game(int height, int width) {
+		if (isStrictlyPositiveInt(height) && isStrictlyPositiveInt(width)) {
+			this.height = height;
+			this.width = width;
+		}
+		else {
+			throw new IllegalArgumentException("Height and width must be strictly positive numbers");
+		}
+	}
+	
+	private boolean isStrictlyPositiveInt(int number) {
+		return number > 0;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	private boolean checkIfTile(int x, int y) {
+		return x >= 0 && x < getWidth() && y >= 0 && y < getHeight();
+	}
 }
+
