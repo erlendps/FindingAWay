c234b30a19c44db5a422574920ed20d2ea541267
Erlend Skaaden <erlendskaaden@gmail.com>
2021-02-24 16:59:11 +0100

    Added standard code

--- project/src/main/java/GUI/FindingAWayApp.java
+++ project/src/main/java/GUI/FindingAWayApp.java
@@ -1,16 +1,23 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
-public class BlockyWalkingApp extends Application {
+
+public class FindingAWayApp extends Application {
 
 	@Override
-	public void start(Stage primaryStage) {
-		
+	public void start(Stage stage) throws Exception {
+		Parent parent = FXMLLoader.load(getClass().getResource("FindingAWay.fxml"));
+		stage.setTitle("Finding a Way");
+		stage.setScene(new Scene(parent));
+        stage.show();
 	}
 
 	public static void main(String[] args) {
-		launch(args);
+		launch(FindingAWayApp.class, args);
 	}
 }
