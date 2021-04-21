1e9df1e5008bb5d30903db87382bd3237d3a4446
Erlend Skaaden <erlendskaaden@gmail.com>
2021-04-21 18:46:24 +0200

    Removed import

--- project/src/main/java/levelEditor/ValidLevelHelper.java
+++ project/src/main/java/levelEditor/ValidLevelHelper.java
@@ -1,27 +1,28 @@
 package levelEditor;
 
+import core.AbstractGame;
 import core.FindingAWay;
 
 public class ValidLevelHelper {
 	
-	public static boolean checkIfValidLevel(FindingAWay level) {
+	public static boolean checkIfValidLevel(AbstractGame level) {
 		if (level == null)
 			return false;
 		return checkIfPlayerAdded(level) && checkIfCollisionUnderPlayer(level)
 				&& checkExactlyOneGoal(level);
 	}
 	
-	private static boolean checkIfPlayerAdded(FindingAWay level) {
+	private static boolean checkIfPlayerAdded(AbstractGame level) {
 		if (level.getPlayerModel() == null)
 			return false;
 		return true;
 	}
 	
-	private static boolean checkIfCollisionUnderPlayer(FindingAWay level) {
+	private static boolean checkIfCollisionUnderPlayer(AbstractGame level) {
 		return level.getTile(level.getPlayerBody().getX(), level.getPlayerBody().getY() + 1).isCollisionBlock();
 	}
 	
-	private static boolean checkExactlyOneGoal(FindingAWay level) {
+	private static boolean checkExactlyOneGoal(AbstractGame level) {
 		int count = 0;
 		for (int y = 0; y < level.getHeight(); y++) {
 			for (int x = 0; x < level.getWidth(); x++) {
