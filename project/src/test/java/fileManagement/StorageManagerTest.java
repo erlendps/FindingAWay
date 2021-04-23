bfd877b9cdfa2e24cd04a52d22efc653524ac866
Erlend Skaaden <erlendskaaden@gmail.com>
2021-04-23 17:40:37 +0200

    Initiated StorageManagerTest

---
+++ project/src/test/java/fileManagement/StorageManagerTest.java
@@ -0,0 +1,11 @@
+package fileManagement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StorageManagerTest {
+
+}
