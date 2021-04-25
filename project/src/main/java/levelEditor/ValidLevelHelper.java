7211061dbaa834adc464e6a6bebe283490eb0bc6
Erlend Skaaden <erlendskaaden@gmail.com>
2021-04-25 15:47:37 +0200

    Commenting

--- project/src/main/java/levelEditor/ValidLevelHelper.java
+++ project/src/main/java/levelEditor/ValidLevelHelper.java
@@ -5,6 +5,7 @@ import core.AbstractGame;
 
 public class ValidLevelHelper {
 	
+	// checks if a Level AbstractGame object is valid
 	public static boolean checkIfValidLevel(AbstractGame level) {
 		if (level == null)
 			return false;
@@ -12,12 +13,16 @@ public class ValidLevelHelper {
 				&& checkExactlyOneFinish(level);
 	}
 	
+	// method that checks wheter player is added
 	private static boolean checkIfPlayerAdded(AbstractGame level) {
 		if (level.getPlayerModel() == null)
 			return false;
 		return true;
 	}
 	
+	// checks that its a collision tile under the player. If the tile under the
+	// player is not a tile, it will return true, so that the game can be saved if
+	// for example the game is over.
 	private static boolean checkIfCollisionUnderPlayer(AbstractGame level) {
 		boolean out;
 		try {
@@ -29,6 +34,9 @@ public class ValidLevelHelper {
 		return out;
 	}
 	
+	// checks that its excayly one finish on the board. It does not mean that there
+	// exists a tile that has type finish, because one could save the game after
+	// a game is won.
 	private static boolean checkExactlyOneFinish(AbstractGame level) {
 		if (level.getFinish() == null)
 			return false;
